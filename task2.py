521a6385380895316a783341dc6971daeea800af
adithimanjunath <56434175+adithimanjunath@users.noreply.github.com>
2020-11-24 17:06:30 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,10 @@
+a=b=c=20
+print("divide a/10-{a/10}\t Multiply b by 50-{b+50}\t Add 50 to c -{c+50})
+
+string="goodmorning"
+print(string[:2]+ "a"+ string[3:])
+
+m=10
+n= 6.7
+print({int(n)} , {float(n)}")
+print({int(m)} , {float(n)}")
