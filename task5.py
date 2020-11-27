3821fe78050e2515d0e66ee7d465a2c1d1f5f3d5
Adithi M Shrouthy <56434175+adithimanjunath@users.noreply.github.com>
2020-11-27 15:26:49 +0530

    Create task5.py

---
+++ task5.py
@@ -0,0 +1,35 @@
+def add(a , b):
+    print("The sum is" , (a+b))
+    
+def sub(a , b):    
+    print("The difference  is" , (a-b))
+    
+def div(a , b):  
+    print("The remainder is" , (a/b))
+    
+def mul(a , b):   
+    print("The productor is" , (a*b))
+    
+add(10,20)
+sub(20,10)
+div(20,10)
+mul(10,20)
+
+//covid
+name = input("enter name")
+temp = 98.6
+def covid(name , temp):
+          print("Patient name is " , name , "and body temperature is ", temp)
+print(covid(name , temp))
+
+    
+   
+  
+ 
+    
+    
+   
+    
+   
+    
+    
