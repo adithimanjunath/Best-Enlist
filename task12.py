2cac5c3b84ce59b75fdb6c85b27fdc9041620267
Adithi M Shrouthy <56434175+adithimanjunath@users.noreply.github.com>
2020-12-04 18:44:41 +0530

    Create task12.py

---
+++ task12.py
@@ -0,0 +1,11 @@
+import json
+
+print(json.dumps({"Name": "Adithi" , "Sem":"7th"}))
+print(json.dumps(["Adithi" , "udanka"]))
+print(json.dumps(("Adithi" , "udanka")))
+print(json.dumps("20"))
+print(json.dumps("20.6"))
+print(json.dumps("Adithi"))
+print(json.dumps("True"))
+print(json.dumps("False"))
+print(json.dumps("None"))
